e1528e2cd59a3c0a2e9daba033e9ff7d26ff7190
Gael Gonzalez <gaeldev032@gmail.com>
2022-07-07 22:52:36 -0600

    📄 Basic page router added - react-router-dom has ben implemented - root page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
+// Dependencies
+import { Route, Routes } from "react-router-dom";
+
+// Components
+import Root from "./pages/Root";
+
 const App = () => {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route exact path="/" element={<Root/>} />
+      </Routes>
+    </>
+  );
 };
 
 export default App;
---
+++ src/pages/Root.js
@@ -0,0 +1,6 @@
+const Root = ()=>{
+
+  return <h1>Root page...</h1>
+}
+
+export default Root
