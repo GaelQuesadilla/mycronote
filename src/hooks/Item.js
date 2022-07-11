c10fdceb489e268868c777c15d89364ca49c10e6
Gael Gonzalez <gaeldev032@gmail.com>
2022-07-11 01:02:48 -0600

    🖋 Improving functions with comments

--- src/hooks/Item.js
+++ src/hooks/Item.js
@@ -2,6 +2,13 @@
 import { useState, useEffect } from "react";
 import { storageModel } from "../models/storage";
 
+
+/**
+ * Hook to get and set the content of a item content 
+ * @param {string} storageName - Name of the item in local storage 
+ * @param {string} itemId  - Id of the item to use
+ * @returns {list<string, function, objects>} content state, set content state, item value
+ */
 export const Item = (storageName, itemId) => {
   const appStorage = new storageModel(storageName);
   const item = appStorage.getItem(itemId);
