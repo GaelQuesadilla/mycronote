b760d641f616f20a5f7588eb809d463bd83ba27b
Gael Gonzalez <gaeldev032@gmail.com>
2022-07-11 02:09:56 -0600

    ⚛ Display content component

---
+++ src/components/displayContent/DisplayContent.js
@@ -0,0 +1,14 @@
+import ReactMarkdown from "react-markdown"
+
+/**
+ * Component to display the content in markdown
+ * @param {object} - content:string 
+ * @returns {Component}  React markdown component with content
+ */
+export const DisplayContent = (props)=>{
+
+  return<ReactMarkdown>{props.content}</ReactMarkdown>
+
+}
+
+DisplayContent()
