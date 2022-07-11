61bf08f904e6ce885b93e2f76a0bd5f0d6289784
Gael Gonzalez <gaeldev032@gmail.com>
2022-07-11 02:51:50 -0600

    ⚛ Implementating storage hook with WorkDisplay component

--- src/components/workDisplay/WorkDisplay.js
+++ src/components/workDisplay/WorkDisplay.js
@@ -4,9 +4,16 @@ import { useState } from "react";
 // Components
 import { InputArea } from "../input/InputArea";
 import { DisplayContent } from "../displayContent/DisplayContent";
+import { UseItem } from "../../hooks/UseItem";
 
-export const WorkDisplay = () => {
-  const [content, setContent] = useState("")
+/**
+ * Component see and make changes
+ * @param {object} props - storageName:string, itemId:string 
+ * @returns {Component} Input and markwdown display
+ */
+export const WorkDisplay = (props) => {
+  const {storageName, itemId} = props
+  const [content, setContent, item] = UseItem(storageName, itemId)
 
   const handleChange = (event)=>{
     setContent(event.target.value)
